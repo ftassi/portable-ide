ade070e5f81134056e03545e9af1bc8f67bb9d36
Francesco Tassi <tassi.francesco@gmail.com>
2021-11-01 01:14:44 +0000

    Share the whole .ssh directory
    Instead of sharing keys, config, known_hosts independently I prefer to
    share the whole .ssh directory. Should be safe enough to try and work
    just fine.
    I keep the ssh agent sharing for now, but it may be not useful anymore.

--- run.sh
+++ run.sh
@@ -5,9 +5,7 @@ docker run --rm -it -h dev \
     -e SSH_AUTH_SOCK=/ssh-agent \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $SSH_AUTH_SOCK:/ssh-agent \
-    -v $HOME/.ssh/config:/home/me/.ssh/config \
-    -v $HOME/.ssh/id_rsa:/home/me/.ssh/id_rsa:ro \
-    -v $HOME/.ssh/id_rsa.pub:/home/me/.ssh/id_rsa.pub:ro \
+    -v $HOME/.ssh:/home/me/.ssh \
     -v $PWD/config:/home/me/dotfiles \
     -v $PWD:/home/me/workspace \
     --name ide \
